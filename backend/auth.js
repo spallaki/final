1e50d3b581b2762d05a9f37d2415b66298d7e7d8
staceykim4695 <staceykim4695@gmail.com>
2017-12-07 15:41:33 -0800

    passport

--- backend/auth.js
+++ backend/auth.js
@@ -4,13 +4,13 @@ var router = express.Router();
 
 module.exports = function(passport, db) {
 
-  router.get('/login', (req, res, next) => {
-    res.render('login')
-  });
-
-  router.get('/register', (req, res, next) => {
-    res.render('register')
-  });
+  // router.get('/login', (req, res, next) => {
+  //   res.render('login')
+  // });
+  //
+  // router.get('/register', (req, res, next) => {
+  //   res.render('register')
+  // });
 
   router.post('/login', passport.authenticate('local', {
     successRedirect: '/main',
