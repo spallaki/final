e60c60d835c9bf0c7738cd52d14b64a9d8e40c78
staceykim4695 <staceykim4695@gmail.com>
2017-12-07 16:44:10 -0800

    passport

--- backend/auth.js
+++ backend/auth.js
@@ -12,26 +12,43 @@ module.exports = function(passport, db) {
   //   res.render('register')
   // });
 
-  router.post('/login', passport.authenticate('local', {
-    successRedirect: '/main',
-    failureRediret: '/login'
-  }));
+  router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user) => {
+      if (err) {
+        res.json({success: false, error: err})
+      } else if (!user) {
+        res.json({success: false, error: 'User not found'});
+      } else {
+        req.logIn(user, err => {
+          if (err) {
+            res.json({success: false})
+          } else {
+            res.json({success: true, user: user});
+          };
+        })
+      }
+    })(req, res, next);
+  });
+
+  // router.post('/login', passport.authenticate('local', {
+  //   successRedirect: '/main',
+  //   failureRediret: '/login'
+  // }));
 
   router.post('/register', (req, res) => {
     db.query('SELECT * FROM users WHERE username = $1', [req.body.username])
       .then((result) => {
         if (result.rows.length > 0) {
-          console.log('Error: Username already exists')
-          res.redirect('/register')
+          res.json({success: false, error: 'Username already exists'})
         } else if (req.body.password !== req.body.password2) {
-          console.log('Error: Passwords do not match')
+          res.json({success: false, error: 'Passwords do not match'})
         } else {
           db.query('INSERT INTO users (username, password, firstName, lastName, email) VALUES ($1, $2, $3, $4, $5)',
           [req.body.username, req.body.password, req.body.firstName, req.body.lastName, req.body.email])
-          .then(() => res.redirect('/login'))
+          .then((result) => res.json({success: true, user: result}))
         }
       })
-      .catch((err) => res.send(err));
+      .catch((err) => res.json({success: false, error: err});
   })
 
   return router;
