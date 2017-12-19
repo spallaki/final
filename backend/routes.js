3ec0c3ad9642f4b7408cb73ee43d709e4a2cb001
staceykim4695 <staceykim4695@gmail.com>
2017-12-19 15:45:59 -0800

    Fixed queries with back ticks

--- backend/routes.js
+++ backend/routes.js
@@ -12,9 +12,9 @@ module.exports = function(db) {
 //expiration date, pharmacy, pharmacy phone, fk_user_id
 
 router.post('/addPicture', (req, res) => {
-  db.query('UPDATE users
+  db.query(`UPDATE users
   SET profile_pic = $1
-  WHERE id = $2', [req.body.user_URI, req.user.id])
+  WHERE id = $2`, [req.body.user_URI, req.user.id])
   .then((result) => res.json({success: true}))
   .catch((error) => res.json({success: false, error: error}))
 })
@@ -24,9 +24,9 @@ router.post('/addRx', (req, res) => {
   // var physician = req.body.physician.charAt(0).toUpperCase() + req.body.name.slice(1);
   var received = req.body.received || null;
   var expiration_date = req.body.expiration_date || null;
-  db.query('INSERT INTO prescriptions
+  db.query(`INSERT INTO prescriptions
   (name, physician, dosage, quantity, type, rx_number, refills, received, expiration_date, pharmacy, pharmacy_phone, fk_user_id)
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)',
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
   [prescriptionName, req.body.physician, req.body.dosage, req.body.quantity, req.body.type, req.body.rx_number, req.body.refills, received, expiration_date, req.body.pharmacy, req.body.pharmacy_phone, req.user.id])
   .then((result) => res.json({success: true}))
   .catch((error) => {
@@ -48,8 +48,8 @@ router.post('/addNote', (req, res) => {
   }
   today = mm + '/' + dd + '/' + yyyy;
 
-  db.query('INSERT INTO notes
-  VALUES($1, $2, $3),' [today, req.body.noteBody, req.body.id])
+  db.query(`INSERT INTO notes
+  VALUES($1, $2, $3)`, [today, req.body.noteBody, req.body.id])
   .then((result) => res.json({success: true}))
   .catch((error) => res.json({success: false, error: error}))
 });
@@ -67,10 +67,10 @@ router.post('/getAllRx', (req, res) => {
 });
 
 router.post('/getRx/:id', (req, res) => {
-  db.query('SELECT
+  db.query(`SELECT
   *
   FROM prescriptions
-  WHERE id = $1', [req.params.id])
+  WHERE id = $1`, [req.params.id])
   .then((result) => res.json({success: true, result: result.rows}))
   .catch((error) => res.json({success: false, error: error}))
   //front end gets all the prescription information, including the id and can access it in the state
@@ -78,20 +78,20 @@ router.post('/getRx/:id', (req, res) => {
 });
 
 router.post('/getProfile', (req, res) => {
-  db.query('SELECT
+  db.query(`SELECT
   firstName, lastName, profile_pic
-  FROM users WHERE id=$1', [req.user.id])
+  FROM users WHERE id=$1`, [req.user.id])
   .then((result) => res.json({success: true, result: result.rows}))
   .catch((error) => res.json({success: false, error: error}))
 })
 
 router.post('/day', (req, res) => {
-  db.query('SELECT
+  db.query(`SELECT
   p.name, r.day, r.set_time
   FROM prescriptions p
   JOIN users u on p.fk_user_id = u.id
   JOIN reminders r on r.fk_prescription_id = p.id
-  WHERE p.fk_user_id = $1', [req.user.id])
+  WHERE p.fk_user_id = $1`, [req.user.id])
   .then((result) => res.json({success: true, result: result.rows}))
   .catch((error) => res.json({success: false, error: error}))
 });
