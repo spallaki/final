a3c33686334ac6be01a313d4a75866cc972f3901
Smitha <smitha.pallaki@gmail.com>
2018-06-06 22:32:51 -0700

    updates to folder structure

--- frontend/store/configureStore.dev.js
+++
@@ -1,9 +0,0 @@
-import { createStore, compose } from 'redux';
-import rootReducer from '../reducers';
-
-export function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-  );
-}
